83d5290bda5513d0a3f407be643e9b13a55ee01a
MSPR666 <2426001123@qq.com>
2023-04-05 23:17:15 +0800

    VSCode下CMake生成文件上传

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main.cpp.obj"
+  "CMakeFiles/main.dir/main.cpp.obj.d"
+  "main.exe"
+  "main.exe.manifest"
+  "main.lib"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "hello main.cpp" << std::endl;
+	return 0;
+}
