0a23add79f777606d5275e7cb19b596d692b5bad
MSPR666 <2426001123@qq.com>
2023-04-07 00:28:19 +0800

    新增thirdCMake

---
+++ thirdCMake/build/CMakeFiles/Demo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Demo.dir/main.cpp.obj"
+  "CMakeFiles/Demo.dir/main.cpp.obj.d"
+  "Demo.exe"
+  "Demo.exe.manifest"
+  "Demo.pdb"
+  "libDemo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
