214cedf254ec34338ae1f283bbfc40fa0726b303
MSPR666 <2426001123@qq.com>
2023-04-08 15:55:50 +0800

    新增调用三方库项目callThirdParties

---
+++ CallThirdParties/main.cpp
@@ -0,0 +1,10 @@
+#include "MathFunctions.h"
+#include<iostream>
+using namespace std;
+
+int main() {
+	double num = 2.0;
+	int exponent = 3.0;
+	cout << power(num, exponent) << endl;
+	return 0;
+}
